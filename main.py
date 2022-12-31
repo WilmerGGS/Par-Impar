6f4b2ebe9e10b87f4afe160aaef0994a0bd03c25
Wilmer Escritorio Principal <wilmergomez010@gmail.com>
2022-12-30 21:35:21 -0500

    Ventana principal realizada

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
+import tkinter as tk
+import os
+
 def main():
     pass
+    class Window_master:
+        raiz = tk.Tk()
 
+        carpeta_principal = os.path.dirname(__file__)
+
+        carpeta_imagenes = os.path.join(carpeta_principal, "images")
+
+        wtotal = raiz.winfo_screenwidth()
+
+        htotal = raiz.winfo_screenheight()
+
+        wwindow = 750
+
+        hvwindow = 400
+
+        pwidth = round(wtotal/2-wwindow/2)
+
+        pheight = round(htotal/2-hvwindow/2)
+
+        raiz.geometry(str(wwindow)+"x"+str(hvwindow)+"+"+str(pwidth)+"+"+str(pheight))
+
+        raiz.configure(background="#5CE1E6")
+
+        raiz.title("Play is free")
+
+        raiz.resizable(0,0)
+
+        raiz.iconbitmap(os.path.join(carpeta_imagenes,"numero-par-impar.ico"))
+
+        #//////////CONFIG////////////////////////////////////////////////
+
+        lb_welcome = tk.Label(
+            
+            raiz,
+            text="""
+            Type a number that is in the range (1-1000)
+                   To validate if it is odd or even
+            """
+        ).grid(column=4, row=1)
+
+        raiz.mainloop()
 
 if __name__ == '__main__':
     main()
